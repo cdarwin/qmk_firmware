82ae4b893850d460a07d77b043dd9a783d3aed28
crd <jason.thigpen@unitedmasters.com>
2020-04-14 14:59:28 -0700

    [keymap] update crd's equinox keymap

--- keyboards/ai03/equinox/keymaps/crd/keymap.c
+++ keyboards/ai03/equinox/keymaps/crd/keymap.c
@@ -14,6 +14,7 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 #include QMK_KEYBOARD_H
+#include "crd.h"
 /*
     K000, K001, K002, K003, K004, K005, K006, K007, K008, K009, K010, K011, \
     K100, K101, K102, K103, K104, K105, K106, K107, K108, K109, K110, K111, \
@@ -21,26 +22,23 @@
     K300, K301, K302,     K304,       K306,       K308,   K309, K310, K311  \
 */
 
-#define KC_CTES RCTL_T(KC_ESC)
-#define KC_BSM1 LT(1, KC_BSPC)
-#define KC_SPM1 LT(1, KC_SPC)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-  [0] = LAYOUT_all( /* Base */
-    KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_GRV, 
-    KC_CTES, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    XXXXXXX, KC_ENT, 
-    KC_LSFT, KC_SLSH, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_RSFT, 
-    KC_LCTL, XXXXXXX, KC_RGUI,          KC_BSM1,          MO(2),            KC_SPM1, KC_RALT, XXXXXXX, KC_RCTL
+  [0] = LAYOUT_wrapper( /* Base */
+    __________________3x6_QWERTY_L1_____________________, __________________3x6_QWERTY_R1_____________________,
+    __________________3x6_QWERTY_L2_____________________, __________________3x6_QWERTY_R2_____________________,
+    KC_LSFT, __________________3x6_QWERTY_L3_____________________, _____________3x5_QWERTY_R3_________________,
+    KC_LCTL, XXXXXXX, _2_THUMB_DEF_L__,          MO(2),            _2_THUMB_DEF_R__, XXXXXXX, KC_RCTL
   ),
-  [1] = LAYOUT_all( /* Extra Keys */
-    _______, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSLS, 
-    _______, _______, KC_HOME, KC_PGDN, KC_PGUP, KC_END,  KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, XXXXXXX, _______, 
-    _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_LBRC, KC_RBRC, _______, 
-    _______, XXXXXXX, _______,          _______,          _______,          _______, _______, _______, _______
+  [1] = LAYOUT_wrapper( /* Extra Keys */
+    __________________3x6_NUM_NAV_L1____________________, __________________3x6_NUM_NAV_R1____________________,
+    __________________3x6_NUM_NAV_L2____________________, __________________3x6_NUM_NAV_R2____________________,
+    _______, __________________3x6_NUM_NAV_L3____________________, _____________3x5_NUM_NAV_R3________________,
+    _______, XXXXXXX, _2_THUMB_LR1_L__,          _______,          _2_THUMB_LR1_R__, XXXXXXX, _______
   ),
-  [2] = LAYOUT_all( /* Num and FN */
-    RESET,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   _______, _______, KC_MINS, KC_EQL,  _______, 
-    _______, KC_VOLD, KC_VOLU, KC_MUTE, _______, _______, _______, _______, KC_SCLN, KC_QUOT, XXXXXXX, _______, 
+  [2] = LAYOUT_wrapper( /* Num and FN */
+    RESET,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   _______, _______, KC_MINS, KC_EQL,  KC_BSLS, 
+    _______, KC_VOLD, KC_VOLU, KC_MUTE, _______, _______, _______, _______, _______, _______, XXXXXXX, KC_QUOT, 
     _______, _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______, _______, _______, _______, 
     _______, XXXXXXX, _______,          _______,          _______,          _______, _______, XXXXXXX, _______
   )
