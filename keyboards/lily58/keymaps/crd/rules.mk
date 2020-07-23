40f763b0ada6ce6e70b833cda222349f0a660b64
crd <darwin@senet.us>
2020-07-23 09:05:50 -0700

    [keymaps] add crd's lily keymap

---
+++ keyboards/lily58/keymaps/crd/rules.mk
@@ -0,0 +1,5 @@
+BOOTLOADER = atmel-dfu # Elite-C
+MOUSEKEY_ENABLE = yes        # Mouse keys
+EXTRAKEY_ENABLE = yes        # Audio control and System control
+RGBLIGHT_ENABLE = no       # Enable WS2812 RGB underlight.
+OLED_DRIVER_ENABLE= no     # OLED display
