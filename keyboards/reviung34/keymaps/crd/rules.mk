4d61061f490a7a33025b6c62193c8cd770435e14
crd <darwin@senet.us>
2021-08-16 08:41:51 -0700

    Add basic home row mods keymap for revuing34

---
+++ keyboards/reviung34/keymaps/crd/rules.mk
@@ -0,0 +1,6 @@
+MCU = atmega32u4
+BOOTLOADER = atmel-dfu
+BOOTMAGIC_ENABLE = lite      # Virtual DIP switch configuration
+EXTRAKEY_ENABLE = yes       # Audio control and System control
+CONSOLE_ENABLE = no        # Console for debug
+COMMAND_ENABLE = no        # Commands for debug and configuration
