65ed0d443d8872432bf22f64cb19a455687f7b3d
crd <darwin@senet.us>
2021-09-12 08:44:20 -0700

    Add my basic userspace

---
+++ users/crd/crd.h
@@ -0,0 +1,31 @@
+#pragma once
+#include QMK_KEYBOARD_H
+
+#include "wrappers.h"
+
+// Thumbs
+#define KC_BSM1 LT(1, KC_BSPC)
+#define KC_SPM2 LT(2, KC_SPC)
+#define KC_ENM3 LT(3, KC_ENT)
+#define KC_TAM3 LT(3, KC_TAB)
+
+// Left-hand home row mods
+#define CTL_A LCTL_T(KC_A)
+#define ALT_S LALT_T(KC_S)
+#define GUI_D LGUI_T(KC_D)
+#define SFT_F LSFT_T(KC_F)
+#define L2_V LT(2, KC_V)
+#define L2_M LT(2, KC_M)
+
+// Right-hand home row mods
+#define SFT_J RSFT_T(KC_J)
+#define GUI_K RGUI_T(KC_K)
+#define ALT_L LALT_T(KC_L)
+#define CTL_SCL RCTL_T(KC_SCLN)
+
+// Convenient Shortcuts
+#define CTL_ESC LCTL_T(KC_ESC)
+#define DC_NXT LSA(KC_DOWN)
+#define DC_PRV LSA(KC_UP)
+#define IJ_BCK LGUI(KC_LBRC)
+#define IJ_FWD LGUI(KC_RBRC)
---
+++ users/crd/wrappers.h
@@ -0,0 +1,38 @@
+#pragma once
+#include "crd.h"
+
+/* Base Layers */
+#define _____________3x5_QWERTY_L1_________________      KC_Q,    KC_W,    KC_E,    KC_R,    KC_T
+#define _____________3x5_QWERTY_L2_________________     CTL_A,   ALT_S,   GUI_D,   SFT_F,    KC_G
+#define _____________3x5_QWERTY_L3_________________      KC_Z,    KC_X,    KC_C,    KC_V,    KC_B
+
+#define _____________3x5_QWERTY_R1_________________      KC_Y,    KC_U,    KC_I,    KC_O,    KC_P
+#define _____________3x5_QWERTY_R2_________________      KC_H,   SFT_J,   GUI_K,   ALT_L, CTL_SCL
+#define _____________3x5_QWERTY_R3_________________      KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH
+
+/*  Navigation */
+#define _____________3x5_____NAV_L1________________   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_SCLN
+#define _____________3x5_____NAV_L2________________   CTL_ESC, KC_LALT, KC_LGUI, KC_LSFT, KC_MINS
+#define _____________3x5_____NAV_L3________________   XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,  KC_GRV
+
+#define _____________3x5_____NAV_R1________________   KC_QUOT, KC_HOME, KC_PGDN, KC_PGUP,  KC_END
+#define _____________3x5_____NAV_R2________________    KC_EQL, KC_LEFT, KC_DOWN,   KC_UP, KC_RGHT
+#define _____________3x5_____NAV_R3________________   KC_BSLS,  DC_NXT,  IJ_BCK,  IJ_FWD,  DC_PRV
+
+/* Numbers */
+#define _____________3x5_____NUM_L1________________   XXXXXXX,    KC_7,    KC_8,    KC_9, KC_SCLN
+#define _____________3x5_____NUM_L2________________    KC_ESC,    KC_4,    KC_5,    KC_6, KC_MINS
+#define _____________3x5_____NUM_L3________________    KC_DOT,    KC_1,    KC_2,    KC_3,  KC_GRV
+
+#define _____________3x5_____NUM_R1________________   KC_QUOT, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX
+#define _____________3x5_____NUM_R2________________    KC_EQL, KC_LSFT, KC_LGUI, KC_LALT, CTL_ESC
+#define _____________3x5_____NUM_R3________________   KC_BSLS,  DC_NXT,  IJ_BCK,  IJ_FWD,  DC_PRV
+
+/* Symbols */
+#define _____________3x5_____SYM_L1________________     RESET, KC_VOLD, KC_MUTE, KC_VOLU, KC_COLN
+#define _____________3x5_____SYM_L2________________     KC_LT, KC_LCBR, KC_LBRC, KC_LPRN, KC_UNDS
+#define _____________3x5_____SYM_L3________________   XXXXXXX, KC_MPRV, KC_MPLY, KC_MNXT, KC_TILD
+
+#define _____________3x5_____SYM_R1________________   KC_QUOT, XXXXXXX, XXXXXXX, XXXXXXX,   RESET
+#define _____________3x5_____SYM_R2________________   KC_PLUS, KC_RPRN, KC_RBRC, KC_RCBR,   KC_GT
+#define _____________3x5_____SYM_R3________________   KC_PIPE, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX
