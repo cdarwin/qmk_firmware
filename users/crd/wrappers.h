c7ebadabad571da50f1527cbfa19cd561f3cbd62
crd <darwin@senet.us>
2021-09-15 17:34:56 -0700

    update dactyl keymap to json

--- users/crd/wrappers.h
+++ users/crd/wrappers.h
@@ -61,6 +61,24 @@
     )
 #define LAYOUT_kyria_base_wrapper(...)       LAYOUT_kyria_base(__VA_ARGS__)
 
+#define XXX KC_NO
+#define LAYOUT_dacmani_base(\
+K00, K01, K02, K03, K04,                         K05, K06, K07, K08, K09,\
+K10, K11, K12, K13, K14,                         K15, K16, K17, K18, K19,\
+K20, K21, K22, K23, K24,                         K25, K26, K27, K28, K29,\
+          K32, K33,                                   K36, K37          \
+)\
+LAYOUT( \
+K00, K01, K02, K03, K04,                         K05, K06, K07, K08, K09,\
+K10, K11, K12, K13, K14,                         K15, K16, K17, K18, K19,\
+K20, K21, K22, K23, K24,                         K25, K26, K27, K28, K29,\
+     XXX, XXX,                                             XXX, XXX, \
+               K32, K33,                         K36, K37, \
+                         XXX, XXX,     XXX, XXX, \
+                         XXX, XXX,     XXX, XXX                          \
+)
+#define LAYOUT_dacmani_base_wrapper(...)       LAYOUT_dacmani_base(__VA_ARGS__)
+
 /* Base Layers */
 #define _____________3x5_QWERTY_L1_________________      KC_Q,    KC_W,    KC_E,    KC_R,    KC_T
 #define _____________3x5_QWERTY_L2_________________     CTL_A,   ALT_S,   GUI_D,   SFT_F,    KC_G
